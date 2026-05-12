3b077340827d3e4ed51b9477ea31f9fa872a8f95
MrSaltyFish <anveshkhode7588@gmail.com>
2026-05-12 21:17:49 +0530

    base next.js file

--- src/routes/client.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { AppShell } from "@/components/app-shell";
-
-export const Route = createFileRoute("/client")({
-  component: () => <AppShell requiredRole="CLIENT"><Outlet /></AppShell>,
-});
--- src/routes/executive.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { AppShell } from "@/components/app-shell";
-
-export const Route = createFileRoute("/executive")({
-  component: () => <AppShell requiredRole="EXECUTIVE"><Outlet /></AppShell>,
-});
